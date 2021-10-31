04653d70b73ae3476f028dbd222b55e576bce556
matthew <matthewmaxwell30@gmail.com>
2021-10-31 18:58:28 -0400

    map for toronto label fully implemented

--- app/src/main/java/com/example/matthewmaxwell_mapd711_assignment3/TorontoMapActivity.kt
+++ app/src/main/java/com/example/matthewmaxwell_mapd711_assignment3/TorontoMapActivity.kt
@@ -2,6 +2,8 @@ package com.example.matthewmaxwell_mapd711_assignment3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,7 +15,7 @@ import com.example.matthewmaxwell_mapd711_assignment3.databinding.ActivityToront
 
 class TorontoMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityTorontoMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,11 +40,38 @@ class TorontoMapActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val toronto = LatLng(43.651070, -79.347015)
+        map.addMarker(MarkerOptions().position(toronto).title("Cruise toronto"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(toronto))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        // Change the map type based on the user's selection.
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
